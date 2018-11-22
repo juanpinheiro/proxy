5022d65f617c46cf1a28b44bc3c79275f3f3c428
Juan Pinheiro <jtlpinheiro@gmail.com>
2018-11-22 10:10:29 -0200

    IP Reset

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var server = 'http://18.204.214.35:3001';
+var server = 'http://52.3.230.109:3001';
  
 app.all('/tracker/:path/:producerId/:campaignId', function(req, res) {
     apiProxy.web(req, res, {target: `${server}`});
