bc1a8d0a9514b3f5c2de15a9a160031ce54fb1d7
Juan Pinheiro <jtlpinheiro@gmail.com>
2018-11-23 10:07:39 -0200

    IP reverse

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var server = 'http://18.204.214.35:3001';
+var server = 'http://52.3.230.109:3001';
  
 app.all('/tracker/:path/:producerId/:campaignId', function(req, res) {
     apiProxy.web(req, res, {target: `${server}`});
 });
 
 app.listen(process.env.PORT || 3000);
-
-
